18661d0138662e55e149bcce2b8a5c2d51add07c
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-10-23 15:24:48 -0400

    Fixed Matrix4 include order issues

--- LibMath/Header/Matrix/Matrix4.h
+++ LibMath/Header/Matrix/Matrix4.h
@@ -57,10 +57,10 @@ namespace LibMath
 	typedef Matrix4x4 Matrix4;
 }
 
+#include "Matrix4.inl"
+
 #ifdef __LIBMATH__VECTOR__VECTOR4_H__
 #include "Matrix4Vector4Operation.h"
 #endif // __LIBMATH__MATRIX_H__
-
-#include "Matrix4.inl"
 
 #endif // !__LIBMATH__MATRIX__MATRIX4_H__
