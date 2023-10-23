e1346f3313b8bc7072481b90a64d76a20500314e
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-10-23 15:24:06 -0400

    Made mat4*vec4 operator conserve vec4's data type

--- LibMath/Header/Matrix4Vector4Operation.h
+++ LibMath/Header/Matrix4Vector4Operation.h
@@ -7,17 +7,17 @@
 namespace LibMath
 {
     template <class T, class U>
-	TVector4<T> operator*(TMatrix<4, 4, T> const& operation, TVector4<U> const& operand)
-	{
-		TMatrix<4, 1, T> vec4Mat;
-		vec4Mat[0] = static_cast<T>(operand.m_x);
-		vec4Mat[1] = static_cast<T>(operand.m_y);
-		vec4Mat[2] = static_cast<T>(operand.m_z);
-		vec4Mat[3] = static_cast<T>(operand.m_w);
+    TVector4<U> operator*(const TMatrix<4, 4, T>& operation, const TVector4<U>& operand)
+    {
+        TMatrix<4, 1, T> vec4Mat;
+        vec4Mat[0] = static_cast<T>(operand.m_x);
+        vec4Mat[1] = static_cast<T>(operand.m_y);
+        vec4Mat[2] = static_cast<T>(operand.m_z);
+        vec4Mat[3] = static_cast<T>(operand.m_w);
 
-		vec4Mat = operation * vec4Mat;
-		return { vec4Mat[0], vec4Mat[1], vec4Mat[2], vec4Mat[3] };
-	}
+        vec4Mat = operation * vec4Mat;
+        return { static_cast<U>(vec4Mat[0]), static_cast<U>(vec4Mat[1]), static_cast<U>(vec4Mat[2]), static_cast<U>(vec4Mat[3]) };
+    }
 }
 
 #endif // !__LIBMATH__MATRIX4VECTOR4OPERATION_H_
