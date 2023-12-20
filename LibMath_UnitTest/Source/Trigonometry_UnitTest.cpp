406d5db248558e47d8106b8f6a399e77e81b8244
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-12-20 02:19:53 -0500

    Refactored unit tests

--- LibMath_UnitTest/Source/Trigonometry_UnitTest.cpp
+++ LibMath_UnitTest/Source/Trigonometry_UnitTest.cpp
@@ -6,14 +6,14 @@
 
 TEST_CASE("Trigonometry", "[.all][trigonometry]")
 {
-	LibMath::Radian radian{ 1.2f };
+    LibMath::Radian radian{ 1.2f };
 
-	CHECK(LibMath::sin(radian) == Catch::Approx(glm::sin(1.2f)));
-	CHECK(LibMath::cos(radian) == Catch::Approx(glm::cos(1.2f)));
-	CHECK(LibMath::tan(radian) == Catch::Approx(glm::tan(1.2f)));
+    CHECK(LibMath::sin(radian) == Catch::Approx(glm::sin(1.2f)));
+    CHECK(LibMath::cos(radian) == Catch::Approx(glm::cos(1.2f)));
+    CHECK(LibMath::tan(radian) == Catch::Approx(glm::tan(1.2f)));
 
-	CHECK(LibMath::asin(0.5f).radian() == Catch::Approx(glm::asin(0.5f)));
-	CHECK(LibMath::acos(0.5f).radian() == Catch::Approx(glm::acos(0.5f)));
-	CHECK(LibMath::atan(0.5f).radian() == Catch::Approx(glm::atan(0.5f)));
-	CHECK(LibMath::atan(1.f, -2.f).radian() == Catch::Approx(glm::atan(1.f, -2.f)));
+    CHECK(LibMath::asin(0.5f).radian() == Catch::Approx(glm::asin(0.5f)));
+    CHECK(LibMath::acos(0.5f).radian() == Catch::Approx(glm::acos(0.5f)));
+    CHECK(LibMath::atan(0.5f).radian() == Catch::Approx(glm::atan(0.5f)));
+    CHECK(LibMath::atan(1.f, -2.f).radian() == Catch::Approx(glm::atan(1.f, -2.f)));
 }
