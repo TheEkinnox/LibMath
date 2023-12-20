d1e0445d8348c98cd10fd8f73b5fca248a53897a
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-12-20 02:49:18 -0500

    Cleaned up matrix template
    - replaced exceptions with static_asserts where possible
    - made row/column count & size getters constexpr
    - updated indentation

--- LibMath/Header/Matrix/TMatrix.h
+++ LibMath/Header/Matrix/TMatrix.h
@@ -5,145 +5,119 @@
 
 namespace LibMath
 {
-	namespace Exceptions
-	{
-		class IncompatibleMatrix : public std::exception
-		{
-		public:
-			IncompatibleMatrix() noexcept
-				: exception()
-			{
-			}
-
-            [[nodiscard]] inline const char* what() const noexcept override
+    namespace Exceptions
+    {
+        class NonInvertibleMatrix : public std::exception
+        {
+        public:
+            NonInvertibleMatrix() noexcept
+                : exception()
             {
-                return "Incompatible matrix";
             }
-		};
-
-		class NonSquareMatrix : public std::exception
-		{
-		public:
-			NonSquareMatrix() noexcept
-				: exception()
-			{
-			}
-
-            [[nodiscard]] inline const char* what() const noexcept override
-            {
-                return "Non-square matrix";
-            }
-		};
-
-		class NonInvertibleMatrix : public std::exception
-		{
-		public:
-			NonInvertibleMatrix() noexcept
-				: exception()
-			{
-			}
 
             [[nodiscard]] inline const char* what() const noexcept override
             {
                 return "Non-invertible matrix";
             }
-		};
-	}
+        };
+    }
 
-	using length_t = int;
+    using length_t = int;
 
-	template<length_t Rows, length_t Cols, typename DataT>
-	class TMatrix
-	{
-		static_assert(std::is_arithmetic_v<DataT>, "Invalid matrix - data type should be an arithmetic type");
-		static_assert(Rows > 0, "Invalid matrix - row count should be greater than or equal to zero");
-		static_assert(Cols > 0, "Invalid matrix - column count should be greater than or equal to zero");
+    template <length_t Rows, length_t Cols, typename DataT>
+    class TMatrix
+    {
+        static_assert(std::is_arithmetic_v<DataT>, "Invalid matrix - data type should be an arithmetic type");
+        static_assert(Rows > 0, "Invalid matrix - row count should be greater than zero");
+        static_assert(Cols > 0, "Invalid matrix - column count should be greater than zero");
 
-	public:
-		constexpr						TMatrix();
-		explicit constexpr				TMatrix(DataT scalar);
-		constexpr						TMatrix(TMatrix const& other);
-		constexpr						TMatrix(TMatrix&& other) noexcept;
-										~TMatrix() = default;
+    public:
+        constexpr          TMatrix();
+        explicit constexpr TMatrix(DataT scalar);
+        constexpr          TMatrix(const TMatrix& other);
+        constexpr          TMatrix(TMatrix&& other) noexcept;
+        ~TMatrix() = default;
 
-		TMatrix&						operator=(TMatrix const& other);
-		TMatrix&						operator=(TMatrix&& other) noexcept;
+        TMatrix& operator=(const TMatrix& other);
+        TMatrix& operator=(TMatrix&& other) noexcept;
 
-										template <typename T>
-										operator TMatrix<Rows, Cols, T>();
+        template <typename T>
+        operator TMatrix<Rows, Cols, T>();
 
-		constexpr DataT					operator[](size_t index) const;
-		DataT&							operator[](size_t index);
+        constexpr DataT operator[](size_t index) const;
+        DataT&          operator[](size_t index);
 
-		DataT							operator()(length_t row, length_t column) const;
-		DataT&							operator()(length_t row, length_t column);
+        DataT  operator()(length_t row, length_t column) const;
+        DataT& operator()(length_t row, length_t column);
 
-		TMatrix&						operator+=(TMatrix const& other);
-		TMatrix&						operator-=(TMatrix const& other);
+        TMatrix& operator+=(const TMatrix& other);
+        TMatrix& operator-=(const TMatrix& other);
 
-		TMatrix&						operator*=(TMatrix const& other);
-		TMatrix&						operator/=(TMatrix const& other);
+        TMatrix& operator*=(const TMatrix& other);
+        TMatrix& operator/=(const TMatrix& other);
 
-		TMatrix&						operator+=(DataT scalar);
-		TMatrix&						operator-=(DataT scalar);
-		TMatrix&						operator*=(DataT scalar);
-		TMatrix&						operator/=(DataT scalar);
+        TMatrix& operator+=(DataT scalar);
+        TMatrix& operator-=(DataT scalar);
+        TMatrix& operator*=(DataT scalar);
+        TMatrix& operator/=(DataT scalar);
 
-		TMatrix							operator+(TMatrix const& other) const;
-		TMatrix							operator-(TMatrix const& other) const;
+        TMatrix operator+(const TMatrix& other) const;
+        TMatrix operator-(const TMatrix& other) const;
 
-		template <length_t OtherRows, length_t OtherCols>
-		TMatrix<Rows, OtherCols, DataT>	operator*(TMatrix<OtherRows, OtherCols, DataT> const& other) const;
-		TMatrix							operator/(TMatrix const& other) const;
+        template <length_t OtherRows, length_t OtherCols>
+        TMatrix<Rows, OtherCols, DataT> operator*(const TMatrix<OtherRows, OtherCols, DataT>& other) const;
+        TMatrix                         operator/(const TMatrix& other) const;
 
-		TMatrix							operator+(DataT scalar) const;
-		TMatrix							operator-(DataT scalar) const;
-		TMatrix							operator*(DataT scalar) const;
-		TMatrix							operator/(DataT scalar) const;
+        TMatrix operator+(DataT scalar) const;
+        TMatrix operator-(DataT scalar) const;
+        TMatrix operator*(DataT scalar) const;
+        TMatrix operator/(DataT scalar) const;
 
-		TMatrix							operator-() const;
+        TMatrix operator-() const;
 
-		constexpr bool					operator==(const TMatrix& other) const;
-		constexpr bool					operator!=(const TMatrix& other) const;
-		bool							isIdentity() const;
+        constexpr bool operator==(const TMatrix& other) const;
+        constexpr bool operator!=(const TMatrix& other) const;
+        bool           isIdentity() const;
 
-		static length_t					getRowCount();
-		static length_t					getColumnCount();
-		static size_t					getSize();
-		static length_t					getIndex(length_t row, length_t column);
-		DataT*							getArray();
-		const DataT*					getArray() const;
+        static constexpr length_t getRowCount();
+        static constexpr length_t getColumnCount();
+        static constexpr size_t   getSize();
 
-		DataT							determinant() const;
-		DataT							cofactor(length_t row, length_t column) const;
+        static length_t getIndex(length_t row, length_t column);
 
-		TMatrix<Rows-1, Cols-1, DataT>	minor(length_t row, length_t column) const;
-		TMatrix<Cols, Rows, DataT>		transposed() const;
-		TMatrix<Rows, Cols, DataT>		coMatrix() const;
-		TMatrix<Cols, Rows, DataT>		adjugate() const;
-		TMatrix<Rows, Cols, DataT>		inverse() const;
+        DataT*       getArray();
+        const DataT* getArray() const;
 
-		std::string string() const;
-		std::string stringLong() const;
+        DataT determinant() const;
+        DataT cofactor(length_t row, length_t column) const;
 
-	private:
-		DataT							m_values[Rows * Cols];
-	};
+        TMatrix<Rows - 1, Cols - 1, DataT> minor(length_t row, length_t column) const;
+        TMatrix<Cols, Rows, DataT>         transposed() const;
+        TMatrix<Rows, Cols, DataT>         coMatrix() const;
+        TMatrix<Cols, Rows, DataT>         adjugate() const;
+        TMatrix<Rows, Cols, DataT>         inverse() const;
 
-	template<length_t Rows, length_t Cols, typename DataT>
-	std::ostream& operator<<(std::ostream& stream, const TMatrix<Rows, Cols, DataT>& mat);
+        std::string string() const;
+        std::string stringLong() const;
 
-	template<length_t Rows, length_t Cols, typename DataT>
-	std::istream& operator>>(std::istream& stream, TMatrix<Rows, Cols, DataT>& mat);
+    private:
+        DataT m_values[Rows * Cols];
+    };
 
-	namespace Details
-	{
-		template<length_t Rows, length_t Cols, typename DataT>
-		struct Determinant
-		{
-			static DataT	compute(const TMatrix<Rows, Cols, DataT>& mat);
-		};
-	}
+    template <length_t Rows, length_t Cols, typename DataT>
+    std::ostream& operator<<(std::ostream& stream, const TMatrix<Rows, Cols, DataT>& mat);
+
+    template <length_t Rows, length_t Cols, typename DataT>
+    std::istream& operator>>(std::istream& stream, TMatrix<Rows, Cols, DataT>& mat);
+
+    namespace Details
+    {
+        template <length_t Rows, length_t Cols, typename DataT>
+        struct Determinant
+        {
+            static DataT compute(const TMatrix<Rows, Cols, DataT>& mat);
+        };
+    }
 }
 
 #include "TMatrix.inl"
